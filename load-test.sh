6344f1dbfe57eddcb6139266af04ec2138432d2e
Sebastian Daschner <git@sebastian-daschner.de>
2018-10-15 06:38:30 +0200

    updated for Istio example

--- load-test.sh
+++ load-test.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 FORTIO_POD=$(kubectl get pods | grep fortio | awk '{ print $1 }')
-HOST=$(kubectl-istio-gateway-ip-address)
-#PORT=$(kubectl-istio-gateway-http-nodeport)
-#HOST='coffee-shop:8080'
-#HOST="192.168.99.100:$PORT"
-#HOST="35.204.171.231"
+HOST=$(kubectl get services -n istio-system istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
-kubectl exec -it $FORTIO_POD  -c fortio /usr/local/bin/fortio -- load -a -c 8 -qps 500 -t 59s -loglevel Warning http://$HOST/coffee-shop/resources/coffee
-#kubectl exec -it $FORTIO_POD  -c fortio /usr/local/bin/fortio -- load -a -c 2 -qps 0 -n 100 -loglevel Warning http://$HOST/coffee-shop/resources/coffee
+kubectl exec -it $FORTIO_POD  -c fortio /usr/local/bin/fortio -- load -a -c 50 -qps 500 -t 20s -loglevel Warning http://$HOST/coffee-shop/resources/coffee
